72efd8f18786beb3628023d529a697344ffdac86
sofia <sofiaisabelleandraderocha@gmail.com>
2026-04-22 22:38:23 -0300

    Criei a model clientes

---
+++ Src/Model/Clientes.js
@@ -0,0 +1,11 @@
+class Cliente{
+    constructor(idCliente, nome, email, Endereco, telefone){
+        this.idCliente= idCliente;
+        this.nome = nome;
+        this.email = email;
+        this.Endereco = Endereco;
+        this.telefone = telefone;
+    }
+}
+
+Module.exports = Cliente;

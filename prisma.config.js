421a7514e37b3b056b774367eca64a900e194adf
wagnermarques <wagnerdocri@gmail.com>
2026-05-27 22:29:41 -0300

    adicionado o env e o prima ao projeto

---
+++ prisma.config.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { defineConfig, env }  from "prisma/config";
+
+export default defineConfig({
+    schema: "prisma/schema.prima",
+    migrations:{
+        path: "prisma/migrations"
+    },
+    datasource:{
+        url: env("DATABASE_URL"),
+    }
+});
